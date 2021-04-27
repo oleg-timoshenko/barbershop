7d18e75592b555878f58285a8f3c4d4cf3cd41bd
Олег Тимошенко <ironhide1900@gmail.com>
2021-04-27 22:53:09 +0300

    v2 commit

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    $('.center').slick({
+        centerMode: false,
+        dots: true,
+        // centerPadding: '60px',
+        slidesToShow: 3,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+});
